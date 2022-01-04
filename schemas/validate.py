5e66b208e7312efe578c96a1e71df2d4dea79d63
Arnaud Leloup <arnaud.leloup@datadoghq.com>
2022-01-04 11:50:49 +0100

    Add custom validation

--- schemas/validate.py
+++ schemas/validate.py
@@ -3,6 +3,7 @@ import json
 import re
 from jsonschema import Draft7Validator, draft7_format_checker, RefResolver
 
+MAX_TAG_LENGTH = 200
 
 def _crawl_schemas():
     for root, _, files in os.walk("schemas"):
@@ -59,6 +60,84 @@ def _validate_with_schema(validator, rule):
     return False
 
 
+def _validate_tags(rule):
+    is_success = True
+
+    for tag_key in rule["tags"]:
+        tag_value = rule["tags"][tag_key]
+        tag = tag_key + tag_value
+
+        if len(tag) > MAX_TAG_LENGTH:
+            print(f"Tag '{tag_key}: {tag_value}' has a length "
+                  f"greater than {MAX_TAG_LENGTH}")
+            is_success = False
+
+        if tag_value.endswith(':'):
+            print(f"Tag '{tag_key}: {tag_value}' has a value ending with ':'")
+            is_success = False
+
+        if re.search(r"[!@#$%^&*()+=\[\]{};'\"|,<>?]", tag):
+            print(f"Tag '{tag_key}: {tag_value}' cannot have "
+                  f"a special character")
+            is_success = False
+
+    return is_success
+
+
+def _validate_conditions(rule):
+    is_success = True
+
+    for condition in rule["conditions"]:
+        operator = condition["operator"]
+
+        if operator == "match_regex":
+            parameters = condition["parameters"]
+
+            if len(parameters["regex"]) == 0:
+                print(f"match_regex condition missing regex field")
+                is_success = False
+
+            if len(parameters["inputs"]) == 0:
+                print(f"match_regex condition missing inputs field")
+                is_success = False
+
+            if "options" in parameters:
+                for option in parameters["options"]:
+                    if option != "case_sensitive" and option != "min_length":
+                        print(f"invalid option for match_regex condition '{option}'")
+                        is_success = False
+
+        if operator == "phrase_match":
+            parameters = condition["parameters"]
+
+            if len(parameters["list"]) == 0:
+                print(f"phrase_match condition missing list field")
+                is_success = False
+
+            if len(parameters["inputs"]) == 0:
+                print(f"phrase_match condition missing inputs field")
+                is_success = False
+
+    return is_success
+
+
+def _validate_custom(rules):
+    # Duplicated from the AppSec service, ingesting the rules
+    # https://github.com/DataDog/dd-go/blob/prod/domains/appsec/apps/appsec/rules.go#L59
+
+    # We only validate currently supported rules
+    if rules["version"] < "2":
+        return True
+
+    is_success = True
+
+    for rule in rules["rules"]:
+        is_success &= _validate_tags(rule)
+        is_success &= _validate_conditions(rule)
+
+    return is_success
+
+
 def main():
 
     validator = _get_schema_validator()
@@ -70,6 +149,7 @@ def main():
 
         print(f"Validating {filename}")
         is_success &= _validate_with_schema(validator, rule)
+        is_success &= _validate_custom(rule)
 
     if not is_success:
         exit(1)
